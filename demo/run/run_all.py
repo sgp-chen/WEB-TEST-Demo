719bfb5fd98afc353a7a642365601cbe50fef218
anchen <623374926@qq.com>
2021-06-17 19:12:12 +0800

    修改logging

--- demo/run/run_all.py
+++ demo/run/run_all.py
@@ -5,8 +5,8 @@ from tools.smtp_post import email_
 
 if __name__ == '__main__':
     discover = unittest.defaultTestLoader.discover("../test_case", 'test*.py', None)
-    localpath = os.path.dirname(os.path.dirname(__file__)).replace("\\", "/")
-    filepath = localpath + "/report"
+    # localpath = os.path.dirname(os.path.dirname(__file__)).replace("\\", "/")
+    filepath = "../report"
     now = datetime.datetime.now().strftime('%Y-%m-%d %H：%M：%S')
     filename = '测试报告 ' + str(now)
     BeautifulReport(discover).report(description='测试', filename=filename, log_path=filepath)
