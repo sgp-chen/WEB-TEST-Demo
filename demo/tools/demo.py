2bb5f92bcac9f4c998b906ed844e14b527226aef
anchen <623374926@qq.com>
2021-06-23 12:20:25 +0800

    增加系统判断

--- demo/tools/demo.py
+++ demo/tools/demo.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from faker import Faker
 import random, string
-
+import platform
 
 class tools():
     url = "http://t-saas.lianduoduo.com/websaas/#"
@@ -66,18 +66,22 @@ class tools():
 
 class Chorme_option():
     def option(self):
-        self.options = webdriver.ChromeOptions()
-        self.options.add_argument('start-maximized')
-        self.options.add_experimental_option('useAutomationExtension', False)
-        self.options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        # root运行且无图形界面
-        self.options.add_argument('--no-sandbox')
-        self.options.add_argument('--disable-dev-shm-usage')
-        self.options.add_argument('--headless')
-        self.options.add_argument('blink-settings=imagesEnabled=false')
-        self.options.add_argument('--disable-gpu')
-        self.prefs = {}
-        self.prefs['credentials_enable_service'] = False
-        self.prefs['profile.password_manager_enabled'] = False
-        self.options.add_experimental_option('prefs', self.prefs)
+        if platform.system()=="Windows":
+            self.options = webdriver.ChromeOptions()
+            self.options.add_argument('start-maximized')
+            self.options.add_experimental_option('useAutomationExtension', False)
+            self.options.add_experimental_option('excludeSwitches', ['enable-automation'])
+            self.prefs = {}
+            self.prefs['credentials_enable_service'] = False
+            self.prefs['profile.password_manager_enabled'] = False
+            self.options.add_experimental_option('prefs', self.prefs)
+        elif platform.system() == "Linux":
+            self.options = webdriver.ChromeOptions()
+            self.options.add_argument('start-maximized')
+            # root运行且无图形界面
+            self.options.add_argument('--no-sandbox')
+            self.options.add_argument('--disable-dev-shm-usage')
+            self.options.add_argument('--headless')
+            self.options.add_argument('blink-settings=imagesEnabled=false')
+            self.options.add_argument('--disable-gpu')
         return self.options
